2254a933720a127c16509e4394ac1229b0a18c8f
tastako307 <tastako307@gmail.com>
2020-12-02 23:10:27 +0900

    ビューの編集１２/2

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+
+  def index
+    # @articles = Article.all
+  end
+  def new
+    # @article = Article.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
