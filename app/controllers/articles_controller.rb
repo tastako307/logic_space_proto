9bb82a62c4443b1031862451d081207daf745599
tastako307 <tastako307@gmail.com>
2020-12-04 00:09:43 +0900

    headerのCSSまで

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,23 +3,23 @@ class ArticlesController < ApplicationController
   def index
     # @articles = Article.all
   end
-  def new
-    # @article = Article.new
-  end
+  # def new
+  #   # @article = Article.new
+  # end
 
-  def show
-  end
+  # def show
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def create
-  end
+  # def create
+  # end
 
-  def update
-  end
+  # def update
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
 
 end
--- db/migrate/20201202095455_create_articles.rb
+++ db/migrate/20201202095455_create_articles.rb
@@ -1,7 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-
+      t.string   :title, null:false
+      t.text     :text,  null:false
       t.timestamps
     end
   end
