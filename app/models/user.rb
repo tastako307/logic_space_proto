9529f9348fd21cf34e7b9edcdaf13c748e4bc86b
tastako307 <tastako307@gmail.com>
2020-12-05 00:44:37 +0900

    ユーザー認証機能の導入

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :email,              uniqueness: { message: '指定したメールアドレスは既に登録されています' }
+  validates :password,           format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, \
+                                           message: 'パスワードには最低でもアルファベットと数字を一文字づつ含めてください' }
+end
