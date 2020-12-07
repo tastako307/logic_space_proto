8e13348ff32b23857bd6033a78dada4bb7921423
tastako307 <tastako307@gmail.com>
2020-12-07 21:59:02 +0900

    今後に備えた記述追加・ビューファイルの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,18 +9,33 @@ class ArticlesController < ApplicationController
   end
 
   # def show
+  # @article = Article.find(params[:id])
   # end
 
   # def edit
+  # @article = Article.find(params[:id])
   # end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+    end
   end
 
   # def update
+  # @article = Article.find(params[:id])
+  # if @article.update(article_params)
+  #  redirect_to 
+  # else
+  # end
   # end
 
-  # def destroy
-  # end
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text).merge(user_id: current_user.id)
+  end
 
 end
