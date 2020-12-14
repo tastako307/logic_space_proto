32582f6ce6cfe46a6ed2b581fb68ad674db38bc7
tastako307 <tastako307@gmail.com>
2020-12-14 23:34:07 +0900

    コメント投稿機能

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
+  validates :text, presence: true,\
+                   length:{ maximum: 150,\
+                   message: 'コメントはは最大で150文字です' }
+
   belongs_to :article
   belongs_to :user
 end
